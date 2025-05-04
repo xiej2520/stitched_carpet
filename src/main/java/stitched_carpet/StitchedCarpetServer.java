b249d55f44133ce10489ca384b6517e160a5bdac
Jacky Xie <jackyxie2520@outlook.com>
2025-05-03 23:56:58 -0500

    Register commands under custom settings manager

--- src/main/java/stitched_carpet/StitchedCarpetServer.java
+++ src/main/java/stitched_carpet/StitchedCarpetServer.java
@@ -2,42 +2,33 @@ package stitched_carpet;
 
 import carpet.CarpetExtension;
 import carpet.CarpetServer;
-import carpet.settings.ParsedRule;
 import carpet.settings.SettingsManager;
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class StitchedCarpetServer implements CarpetExtension, ModInitializer {
-    public static SettingsManager settingsManager;
     public static final Logger LOGGER = LogManager.getLogger(StitchedCarpetServer.class);
-    public static final String MOD_ID = "stitched_carpet";
+    public static final String MOD_ID = "stitch";
     public static final String FANCY_NAME = "StitchedCarpet";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.1.0";
+    public static SettingsManager settingsManager;
 
     @Override
     public void onInitialize() {
-        CarpetServer.manageExtension(new StitchedCarpetServer());
+        settingsManager = new SettingsManager(VERSION, MOD_ID, FANCY_NAME);
+        CarpetServer.manageExtension(this);
     }
 
     @Override
     public void onGameStarted() {
-        // create custom settingsManager
-        settingsManager = new SettingsManager(VERSION, MOD_ID, FANCY_NAME);
         settingsManager.parseSettingsClass(StitchedCarpetSettings.class);
+        LOGGER.info("Stitched Carpet loaded!");
+    }
 
-        CarpetServer.settingsManager.parseSettingsClass(StitchedCarpetSettings.class);
-
-        // workaround for rule being overwritten: https://github.com/gnembon/fabric-carpet/issues/802
-        CarpetServer.settingsManager.addRuleObserver((source, rule, value) -> {
-            ParsedRule<?> stitchedCarpetRule = settingsManager.getRule(rule.name);
-            ParsedRule<?> carpetRule = CarpetServer.settingsManager.getRule(rule.name);
-
-            // check if the rule being changed exists in minitweaks, but isn't the same rule as the one in carpet's settingsManager
-            // if so, update the rule (if types are the same)
-            if(stitchedCarpetRule != null && carpetRule != null && stitchedCarpetRule != carpetRule && stitchedCarpetRule.type == carpetRule.type) {
-                stitchedCarpetRule.set(source, value);
-            }
-        });
+    @Override
+    public SettingsManager customSettingsManager() {
+        // this will ensure that our settings are loaded properly when world loads
+        return settingsManager;
     }
 }
